c848a8c23027840060c6aac46f202d5ee11e643d
tnphuctruong <52196656+tnphuctruong@users.noreply.github.com>
2020-12-04 17:06:21 +0700

    delete item

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -6,19 +6,20 @@ import Col from 'react-bootstrap/Col';
 import './TaskItem.css';
 
 export default class TaskItem extends Component {
-	state = {
-		name: "",
-		time: "",
-		date: null
-	};
+	state = this.props.info
+
+	static getDerivedStateFromProps(props, state) {
+		return props.info;
+	}
 
 	render() {
 		const titleRendered = (this.state.name && this.state.time);
+		const taskIndex = this.props.index;
 		return (
 			<Form className="taskItem">
 				{/* If task does not have full info */}
 				<Form.Label srOnly={titleRendered}>
-					<h4>Chưa đặt tên</h4>
+				<h4>Chưa đặt tên {this.state.name}</h4>
 				</Form.Label>
 				{/* Render task with working hours */}
 				<Form.Label srOnly={!titleRendered}>
@@ -29,18 +30,25 @@ export default class TaskItem extends Component {
 						<Form.Text>Task:</Form.Text>
 						<Form.Control placeholder="Nhập tên task"
 										value={this.state.name}
-										onChange={(e) => this.setState({name: e.target.value})}/>
+										onChange={(e) => {
+												this.setState({name: e.target.value});
+												this.props.onupdate(taskIndex, this.state);
+										}}/>
 					</Col>
 					<Col lg={4}>
 						<Form.Text>Thời gian làm task:</Form.Text>
 						<Form.Control type="number"
 										placeholder="Nhập thời gian làm task"
 										value={this.state.time}
-										onChange={(e) => this.setState({time: Number(e.target.value)})}/>
+										onChange={(e) => {
+											this.setState({time: Number(e.target.value)});
+											this.props.onupdate(taskIndex, this.state);
+											}}
+										/>
 					</Col>
 					<Col lg={2}>
 						<Form.Text>&nbsp;</Form.Text>
-						<Button variant="danger" onClick={(e) => console.log('You clicked!!!')}>Xóa</Button>{' '}
+						<Button variant="danger" onClick={(e) => {this.props.ondelete(taskIndex)}}>Xóa</Button>{' '}
 					</Col>
 				</Form.Row>
 			</Form>
--- src/components/TaskManage.jsx
+++ src/components/TaskManage.jsx
@@ -18,15 +18,33 @@ export default class TaskManage extends Component {
 					</Col>
 				</Row>
 				<div style={{padding: "0.3em 0"}}>
-					{this.state.tasks.map((item, index) => <TaskItem key={index} />)}
+					{this.state.tasks.map((item, index) => 
+							<TaskItem key={index}
+										index={index}
+										info={item}
+										onupdate={this.hdlUpdateItem}
+										ondelete={this.hdlDeleteItem}/>
+					)}
 				</div>
 			</Container>
 		);
 	}
 
-	addTask = (e) => {
+	addTask = () => {
 		var tasks = this.state.tasks;
-		tasks.push({name:"", time: "", date: null});
+		tasks.push({name: tasks.length, time: "", date: null});
+		this.setState({tasks: tasks});
+	}
+
+	hdlDeleteItem = (index) => {
+		var tasks = this.state.tasks;
+		tasks.splice(index, 1);
+		this.setState({tasks: tasks});
+	}
+
+	hdlUpdateItem = (index, item) => {
+		var tasks = this.state.tasks;
+		tasks[index] = item;
 		this.setState({tasks: tasks});
 	}
 }
